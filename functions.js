2ca36b029b902be39301223b1324fb5730f9c888
manavjuthani <155020180+manavjuthani@users.noreply.github.com>
2024-01-10 18:50:56 -0800

    Add files via upload

--- functions.js
+++ functions.js
@@ -1,37 +1,25 @@
 function toggleMenu() {
-    var navLinks = document.querySelector('.nav-links'); // The navigation links container
-    var menuBtn = document.querySelector('.menu-btn'); // The button to open the menu
-    var overlay = document.querySelector('.ham-overlay'); // The overlay
+    const navLinks = document.querySelector('.nav-links');
+    const hamOverlay = document.querySelector('.ham-overlay');
+    const allElements = document.querySelectorAll('body *:not(.nav-links, .nav-links *, .menu-button)');
+    const menuButton = document.querySelector('.menu-button');
 
-    if (navLinks.style.display === 'block' || getComputedStyle(navLinks).display === 'block') {
-        // If the navigation links are currently displayed, hide them
-        navLinks.style.display = 'none';
-        // Hide the overlay
-        overlay.style.display = 'none';
-        // Show the menu button (hamburger icon)
-        menuBtn.style.display = 'block';
+    // Toggle the display of the navigation links and overlay
+    navLinks.style.display = navLinks.style.display === 'block' ? 'none' : 'block';
+    hamOverlay.style.display = hamOverlay.style.display === 'block' ? 'none' : 'block';
+
+    // Change menu button to 'X' if nav links are visible, or back to menu symbol if not
+    if (navLinks.style.display === 'block') {
+        menuButton.innerHTML = '✕'; // Change to 'X'
+        menuButton.style.color = 'white'
+        allElements.forEach(element => {
+            element.style.pointerEvents = 'none';
+        });
     } else {
-        // If the navigation links are currently hidden, display them
-        navLinks.style.display = 'block';
-        // Show the overlay
-        overlay.style.display = 'block';
-        // Hide the menu button (hamburger icon)
-        menuBtn.style.display = 'none';
+        menuButton.innerHTML = '☰'; // Change back to menu symbol
+        menuButton.style.color = 'black'
+        allElements.forEach(element => {
+            element.style.pointerEvents = 'auto';
+        });
     }
 }
-
-// Hide the navigation links and overlay by default
-document.addEventListener('DOMContentLoaded', function () {
-    var navLinks = document.querySelector('.nav-links');
-    var overlay = document.querySelector('.overlay');
-    navLinks.style.display = 'none';
-    overlay.style.display = 'none';
-});
-
-// Add event listener for the hamburger menu button
-var menuBtn = document.querySelector('.menu-btn');
-menuBtn.addEventListener('click', toggleMenu);
-
-// Add event listener to the menu button
-document.querySelector('.menu-button').addEventListener('click', toggleOverlay);
-
